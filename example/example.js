7c97ab4153a826095f5fce70479e5c3fbbf8d170
Evan Tahler <evantahler@gmail.com>
2015-03-19 12:49:52 +0000

    update incomming queue name; driver is also a worker

--- example/example.js
+++ example/example.js
@@ -5,7 +5,6 @@
 var BusPrototype       = require(__dirname + '/../index.js').bus;
 var DriverPrototype    = require(__dirname + '/../index.js').driver;
 var SchedulerPrototype = require("node-resque").scheduler;
-var WorkerPrototype    = require("node-resque").worker;
 
 ///////////////////////////
 // SET UP THE CONNECTION //
@@ -68,19 +67,18 @@ var bus = new BusPrototype({connection: connectionDetails}, jobs, function(){
   // START DRIVER //
   //////////////////
 
-  var driver = new DriverPrototype({connection: connectionDetails}, jobs, function(){
+  // a driver is just like a normal node-resque worker, but will also work the incomming queues when idle
+  var driver = new DriverPrototype({connection: connectionDetails, queues: [bus_queue]}, jobs, function(){
     driver.workerCleanup(); // optional: cleanup any previous improperly shutdown workers
     driver.start();
   });
 
-  ////////////////////
-  // START A WORKER //
-  ////////////////////
-
-  worker = new WorkerPrototype({connection: connectionDetails, queues: [bus_queue]}, jobs, function(){
-    worker.workerCleanup(); // optional: cleanup any previous improperly shutdown workers
-    worker.start();
-  });
+  // converly, you could also just start a worker:
+  // var WorkerPrototype = require("node-resque").worker;
+  // worker = new WorkerPrototype({connection: connectionDetails, queues: [bus_queue]}, jobs, function(){
+  //   worker.workerCleanup(); // optional: cleanup any previous improperly shutdown workers
+  //   worker.start();
+  // });
 
   /////////////////////////
   // REGESTER FOR EVENTS //
@@ -96,17 +94,6 @@ var bus = new BusPrototype({connection: connectionDetails}, jobs, function(){
   driver.on('failure',         function(queue, job, failure){ console.log("job failure " + queue + " " + JSON.stringify(job) + " >> " + failure); });
   driver.on('error',           function(queue, job, error){ console.log("error " + queue + " " + JSON.stringify(job) + " >> " + error); });
   driver.on('pause',           function(){ console.log("worker paused"); });
-
-  worker.on('start',           function(){ console.log("worker started"); });
-  worker.on('end',             function(){ console.log("worker ended"); });
-  worker.on('cleaning_worker', function(worker, pid){ console.log("cleaning old worker " + worker); });
-  worker.on('poll',            function(queue){ console.log("worker polling " + queue); });
-  worker.on('job',             function(queue, job){ console.log("working job " + queue + " " + JSON.stringify(job)); });
-  worker.on('reEnqueue',       function(queue, job, plugin){ console.log("reEnqueue job (" + plugin + ") " + queue + " " + JSON.stringify(job)); });
-  worker.on('success',         function(queue, job, result){ console.log("job success " + queue + " " + JSON.stringify(job) + " >> " + result); });
-  worker.on('failure',         function(queue, job, failure){ console.log("job failure " + queue + " " + JSON.stringify(job) + " >> " + failure); });
-  worker.on('error',           function(queue, job, error){ console.log("error " + queue + " " + JSON.stringify(job) + " >> " + error); });
-  worker.on('pause',           function(){ console.log("worker paused"); });
 
   scheduler.on('start',             function(){ console.log("scheduler started"); });
   scheduler.on('end',               function(){ console.log("scheduler ended"); });
